e152f7268f354ccb329433e1c29abfd232078cec
alchemy-x <134489593+alchemy-x@users.noreply.github.com>
2023-11-24 19:43:03 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/senate/index.html"],
+  theme: {
+    extend: {
+      fontSize: {
+      mammoth:  '8rem',
+      },
+       colors: {
+      primary:  '#687EFF',
+      achena: {
+        100:'#40b3cc',
+        200:'#982176',
+        300:'#d1d7d9',
+        sharon:' #00539C',
+        ele: '#EEA47F',
+        Gloria:' #FBEAEB'
+        
+      }
+  
+      },
+      fontFamily:{
+        ace :['Montserrat, sans-serif'
+]
+      }
+    }
+  }
+}
+
